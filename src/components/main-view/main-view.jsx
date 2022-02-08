58e6f56a0b77b49ec97cb0666f6658690d45a130
Melody Kao <melody.ch.kao@gmail.com>
2022-02-08 10:42:52 +0800

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import {MovieCard} from  '../movie-card/movie-card';
 
- export class MainView extends React.Component {
+import React from 'react';
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
+
+export class MainView extends React.Component {
     constructor(){
         super();
         this.state = {
@@ -9,21 +11,36 @@ import {MovieCard} from  '../movie-card/movie-card';
             { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...'},
             { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...'},
             { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...'}
-          ]
+          ],
+          selectedMovie: null
         }
-      }
+    }
+      
+
+  setSelectedMovie(newSelectedMovie) {
+    this.setState({
+      selectedMovie: newSelectedMovie
+    });
+  }
+    
+
+  render() {
+    const { movies, selectedMovie } = this.state;
 
 
-      render() {
-        const { movies } = this.state;
-      
-        if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
-      
-        return (
-            <div className="main-view">
-                {movies.map(movie=> <MovieCard/>)}
-            </div>
-        );
-      }
+    if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+
+    return (
+      <div className="main-view">
+        {selectedMovie
+          ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+          : movies.map(movie => (
+            <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
+          ))
+        }
+      </div>
+    );
+  }
+}
 
 export default MainView;
