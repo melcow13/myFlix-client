214a499b5e10caf8c778b5035ee2f0a8e3fa8f75
Melody Kao <melody.ch.kao@gmail.com>
2022-02-10 15:21:46 +0800

    add from the main

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,30 +2,68 @@
 import React from 'react';
 import axios from 'axios';
 
+<<<<<<< HEAD
+=======
+import { LoginView } from '../login-view/login-view';
+>>>>>>> main
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+
 
 export class MainView extends React.Component {
     constructor(){
         super();
         this.state = {
           movies: [],
+<<<<<<< HEAD
           selectedMovie: null
+=======
+          selectedMovie: null,
+          user: null
+>>>>>>> main
         }
     }
+  
+    componentDidMount(){
+      axios.get('https://myflixerupper.herokuapp.com/movies')
+      .then(response =>{
+        this.setState(
+          {
+            movies:response.data
+          });
+      })
+        .catch(error=>{
+          console.log(error);
+        });
+    }
       
+<<<<<<< HEAD
   setSelectedMovie(newSelectedMovie) {
     this.setState({
       selectedMovie: newSelectedMovie
     });
   }
+=======
+    setSelectedMovie(newSelectedMovie) {
+      this.setState({
+        selectedMovie: newSelectedMovie
+      });
+    }
+
+    onLoggedIn(user){
+      this.setState({
+        user
+      });
+    }
+>>>>>>> main
     
 
   render() {
-    const { movies, selectedMovie } = this.state;
+    const { movies, selectedMovie, user} = this.state;
 
+    if(!user) return<LoginView on LogggedIn={user=>this.onLoggedIn(user)} />;
 
-    if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+    if (movies.length === 0) return <div className="main-view"/>;
 
     return (
       <div className="main-view">
