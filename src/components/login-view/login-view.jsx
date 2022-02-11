a717ee47e09b07ceb52c501b1cae3806a26baccc
Melody Kao <melody.ch.kao@gmail.com>
2022-02-11 16:07:46 +0800

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,4 +1,6 @@
 import React, {useState} from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 export function LoginView(props){
     const [ username, setUsername ] = useState('');
@@ -11,17 +13,17 @@ export function LoginView(props){
       };
 
     return(
-        <form>
-            <label>
-                Username:
-                <input type="text" value={username} onChange={e=>setUsername(e.target.value)}/>
-            </label>
-            <label>
+        <Form>
+            <Form.Group controlID="formUsername">
+                <Form.Label>Username:</Form.Label> 
+                <Form.Control type="text" value={username} onChange={e=>setUsername(e.target.value)}/>
+            </Form.Group>
+            <Form.Group controlID="formPass">
                 Password:
-                <input type="password" value={password} onChange ={e=>setPassword(e.target.value)}/>
-            </label>
-            <button type="submit" onClick={handleSubmit}>Submit</button>
-            <button>Register</button>
-        </form>
+                <Form.Control type="password" value={password} onChange ={e=>setPassword(e.target.value)}/>
+            </Form.Group>
+            <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
+            <Button>Register</Button>
+        </Form>
     );
 }
