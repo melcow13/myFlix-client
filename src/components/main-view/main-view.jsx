8e4eeab40dd37fed0662e6bd08f4919d91464060
Melody Kao <melody.ch.kao@gmail.com>
2022-02-14 17:11:52 +0800

    pdated code of onLoggedIn and componentdidMount

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -22,17 +22,29 @@ export class MainView extends React.Component {
         }
     }
   
-    componentDidMount(){
-      axios.get('https://myflixerupper.herokuapp.com/movies')
-      .then(response =>{
-        this.setState(
-          {
-            movies:response.data
-          });
+    getMovies(token) {
+      axios.get('https://myflixerupper.herokuapp.com//movies', {
+        headers: { Authorization: `Bearer ${token}`}
       })
-        .catch(error=>{
-          console.log(error);
+      .then(response => {
+        // Assign the result to the state
+        this.setState({
+          movies: response.data
         });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    }
+
+    componentDidMount() {
+      let accessToken = localStorage.getItem('token');
+      if (accessToken !== null) {
+        this.setState({
+          user: localStorage.getItem('user')
+        });
+        this.getMovies(accessToken);
+      }
     }
       
     setSelectedMovie(newSelectedMovie) {
@@ -41,10 +53,15 @@ export class MainView extends React.Component {
       });
     }
 
-    onLoggedIn(user){
+    onLoggedIn(authData) {
+      console.log(authData);
       this.setState({
-        user
+        user: authData.user.Username
       });
+    
+      localStorage.setItem('token', authData.token);
+      localStorage.setItem('user', authData.user.Username);
+      this.getMovies(authData.token);
     }
 
     
