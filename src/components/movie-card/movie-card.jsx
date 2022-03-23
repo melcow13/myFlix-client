038363d4955e506cad62b94f39ee2f69d11cf61a
Melody Kao <melody.ch.kao@gmail.com>
2022-03-23 10:24:16 +0800

    add addFavorites

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Button , Card} from 'react-bootstrap';
+import axios from 'axios';
 
 import { Link } from 'react-router-dom';
 
 export class MovieCard extends React.Component {
   render() {
-    const { movie } = this.props;
+    const { movie, addFavorites } = this.props;
+    const Username = localStorage.getItem('user');
+    const token = localStorage.getItem('token');
+    console.log(token, 'token');
+    const addtoFavorites = (e) => {
+      e.preventDefault();
+      console.log('add to Favourite movies');
+      axios.post(`https://myflixerupper.herokuapp.com/users/${Username}/favorite/${movie._id}`, {},
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        .then(response => {
+          console.log(response.data);
+          alert("movie added to favourites");
+          console.log(movie._id, 'movie id');
+          addFavorites(movie._id);
+          console.log('after movie id');
+        })
+        .catch(e => {
+          console.log('error adding movie to favourites');
+          alert('movie NOT added to favourites');
+        });
+    };
+
 
     return (
       <Card>
@@ -17,6 +41,14 @@ export class MovieCard extends React.Component {
             <Link to={`/movies/${movie._id}`}>
             <Button variant="link">Open</Button>
           </Link>
+          <Button className="button_card_main-view"
+                    style={{ margin: "10px" }}
+                    size="sm"
+                    type="submit"
+                    variant="outline-info"
+                    onClick={addtoFavorites}>
+                    Favorite it
+                  </Button>
           </Card.Body>
       </Card>
     );
